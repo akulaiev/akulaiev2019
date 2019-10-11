d4dff621f1c788b983fda1be9732e2012729ed93
akulaiev <kulaeva.a@ukr.net>
2019-10-12 00:37:07 +0300

    rebuild

--- Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/Objects-normal/x86_64/akulaiev2019-dummy.d
+++ Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/Objects-normal/x86_64/akulaiev2019-dummy.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/DruidKuma/Desktop/SwiftPool/day08/akulaiev2019/Example/Pods/Target\ Support\ Files/akulaiev2019/akulaiev2019-dummy.m \
-  /Users/DruidKuma/Desktop/SwiftPool/day08/akulaiev2019/Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/module.modulemap \
-  /Users/DruidKuma/Desktop/SwiftPool/day08/akulaiev2019/Example/Pods/Target\ Support\ Files/akulaiev2019/akulaiev2019-prefix.pch
+  /Users/DruidKuma/Desktop/akulaiev2019/Example/Pods/Target\ Support\ Files/akulaiev2019/akulaiev2019-dummy.m \
+  /Users/DruidKuma/Desktop/akulaiev2019/Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/module.modulemap \
+  /Users/DruidKuma/Desktop/akulaiev2019/Example/Pods/Target\ Support\ Files/akulaiev2019/akulaiev2019-prefix.pch
--- Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/Objects-normal/x86_64/akulaiev2019_vers.d
+++ Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/Objects-normal/x86_64/akulaiev2019_vers.d
@@ -1,2 +1,2 @@
 dependencies: \
-  /Users/DruidKuma/Desktop/SwiftPool/day08/akulaiev2019/Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/DerivedSources/akulaiev2019_vers.c
+  /Users/DruidKuma/Desktop/akulaiev2019/Example/build/Pods.build/Debug-iphonesimulator/akulaiev2019.build/DerivedSources/akulaiev2019_vers.c
